1ac00ef54dc040b5a4303cd51d5d4d376a4637ae
elnikov.s <elnikov.s@gmail.com>
2022-01-27 11:34:27 +0300

    timecop

--- Gemfile
+++ Gemfile
@@ -40,6 +40,10 @@ group :development, :test do
   gem 'rspec-grape'
 end
 
+group :test do
+  gem 'timecop'
+end
+
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 4.1.0'
--- spec/services/sumsub/generate_url_spec.rb
+++ spec/services/sumsub/generate_url_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 describe SumSub::GenerateUrl do
   describe 'generate link' do
     before do
+      described_class.clear_cache
       @user_id = rand(99999)
       stub_websdk_link(user_id: @user_id)
     end
@@ -35,6 +36,12 @@ describe SumSub::GenerateUrl do
       generate_url =  described_class.new(user_id: @user_id)
       url3 = generate_url.call
       expect(url2).not_to eq(url3)
+
+      Timecop.travel(Time.now+described_class::CACHE_TIME)
+      stub_websdk_link(user_id: @user_id)
+      generate_url =  described_class.new(user_id: @user_id)
+      url4 = generate_url.call
+      expect(url3).not_to eq(url4)
     end
 
   end
