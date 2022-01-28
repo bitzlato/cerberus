ecf7fb1c3d1c9f5329e03b3bdf56fbfcf4f7e547
elnikov.s <elnikov.s@gmail.com>
2022-01-28 14:56:23 +0300

    add procfile

--- config/initializers/sumsub.rb
+++ config/initializers/sumsub.rb
@@ -1,5 +1,5 @@
 Sumsub.configure do |config|
   config.token = ENV.fetch('SUMSUB_TOKEN', '')
   config.secret_key = ENV.fetch('SUMSUB_SECRET', '')
-  config.production = false # is true by default
+  config.production = ENV.fetch('SUMSUB_PRODUCTION', false)
 end
