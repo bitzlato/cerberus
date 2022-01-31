44b3b394aabc43c190a19303720342515c321226
elnikov.s <elnikov.s@gmail.com>
2022-01-31 12:17:10 +0300

    fix boolean cast

--- config/initializers/sumsub.rb
+++ config/initializers/sumsub.rb
@@ -1,5 +1,5 @@
 Sumsub.configure do |config|
   config.token = ENV.fetch('SUMSUB_TOKEN', '')
   config.secret_key = ENV.fetch('SUMSUB_SECRET', '')
-  config.production = ENV.fetch('SUMSUB_PRODUCTION', false)
+  config.production = ActiveModel::Type::Boolean.new.cast(ENV.fetch('SUMSUB_PRODUCTION', false))
 end
