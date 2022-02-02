eec59c9250d48edfe75756da8bace3610132d786
elnikov.s <elnikov.s@gmail.com>
2022-02-02 12:30:09 +0300

    rescue from standart  error

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,12 +2,17 @@ class API < Grape::API
   format :json
   prefix 'api'
 
+
   rescue_from ActiveRecord::RecordNotFound do |e|
     error_response(message: e.message, status: 404)
   end
 
   rescue_from ActiveRecord::RecordInvalid do |e|
     error_response(message: e.message, status: 422)
+  end
+
+  rescue_from StandardError do |e|
+    error_response(message: e.message, status: 500)
   end
 
   helpers ::CurrentUser
