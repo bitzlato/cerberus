d3b6c6f6645de25208dd5ab221771588299e9607
elnikov <elnikov.s@gmail.com>
2022-02-03 16:26:13 +0300

    Feature/real test fixes (#4)
    * add webhook logger
    
    * refactor test functions

---
+++ app/models/concerns/test_webhook.rb
@@ -0,0 +1,49 @@
+module TestWebhook
+  extend ActiveSupport::Concern
+  included do
+    def create_appicant
+      raise 'Cant use test webhook API in production' if Rails.env.production?
+      Sumsub::Request.new.create_applicant('basic-kyc-level', {'externalUserId': rand(9999)})
+
+    end
+
+    def test_webhook(status)
+      raise 'Cant use test webhook API in production' if Rails.env.production?
+
+      review_result = verified_review if status == 'verified'
+      review_result = rejected_review if status == 'rejected'
+      review_result = final_review    if status == 'banned'
+      Sumsub::Request.new.testing_on_test_environment(applicant_id, review_result)
+    end
+
+
+    private
+
+    def rejected_review
+      {
+        "reviewAnswer": "RED",
+        "moderationComment": "We do not accept screenshots. Please upload an original photo.",
+        "clientComment": "Screenshots are not accepted.",
+        "reviewRejectType": "RETRY",
+        "rejectLabels": ["UNSATISFACTORY_PHOTOS","SCREENSHOTS"]
+      }
+    end
+
+    def final_review
+      {
+        "reviewAnswer": "RED",
+        "moderationComment": "We do not accept screenshots. Please upload an original photo.",
+        "clientComment": "Screenshots are not accepted.",
+        "reviewRejectType": "FINAL",
+        "rejectLabels": ["UNSATISFACTORY_PHOTOS","SCREENSHOTS"]
+      }
+    end
+
+    def verified_review
+      {
+        "reviewAnswer": "GREEN",
+        "rejectLabels": []
+      }
+    end
+  end
+end
