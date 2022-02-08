b11c1ca18929e554b14b70e346a9a9cd66c05372
elnikov <elnikov.s@gmail.com>
2022-02-08 09:43:46 +0300

    convert to slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'applicant/index'
+  get 'applicant/show'
   mount API => '/'
+
+  namespace :admin do
+    resources :applicant, only: [:show, :index]
+  end
 end
