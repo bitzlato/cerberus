3b9969be1b9c89476408c030efca8234398fdad3
elnikov <elnikov.s@gmail.com>
2022-02-09 09:54:02 +0300

    #181171559 add default target

--- app/services/currency_convert.rb
+++ app/services/currency_convert.rb
@@ -11,8 +11,8 @@ module CurrencyConvert
   def rates(target: 'USD')
     Rails.cache.fetch('usd_convert_rate', expire_in: RATE_CACHE_EXPIRE ) do
       uri = URI("https://account.bitzlato.com/api/public/v1/rates?target_currency=#{target}")
-      JSON.parse(Net::HTTP.get(uri)).dig('rates')&.with_indifferent_access
-    end
+      JSON.parse(Net::HTTP.get(uri)).dig('rates')
+    end&.with_indifferent_access
   end
 
   end
