48ead808f7f45dc4ccebdef7e22c7d6b4790cb7b
elnikov <elnikov.s@gmail.com>
2022-02-09 09:25:16 +0300

    #181171559 refactor

--- app/services/currency_convert.rb
+++ app/services/currency_convert.rb
@@ -1,21 +1,18 @@
-module ConvertRate
+module CurrencyConvert
   RATE_CACHE_EXPIRE = 10.second
-  class << self
 
+  class << self
     def initialize
     end
-
 
     def call
     end
 
-
-    def get_rates
+    def rates
       Rails.cache.fetch('usd_convert_rate', expire_in: RATE_CACHE_EXPIRE ) do
         uri = URI('https://account.bitzlato.com/api/public/v1/rates?target_currency=USD')
-        JSON.parse(Net::HTTP.get(uri)).dig('rates')
+        JSON.parse(Net::HTTP.get(uri)).dig('rates')&.with_indifferent_access
       end
     end
-
   end
 end
