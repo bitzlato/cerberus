2c54308c62d00ee58b1667e4b8b4ee9c62d0d83b
elnikov <elnikov.s@gmail.com>
2022-02-09 10:00:08 +0300

    #181171559 upcase target

--- spec/services/currency_convert_spec.rb
+++ spec/services/currency_convert_spec.rb
@@ -6,8 +6,6 @@ describe CurrencyConvert do
     let! (:target) { 'USD' }
     let! (:rates) { stub_rates(target: target) }
 
-    it 'ok' do
-      expect(described_class.rates(target: target)).to be_truthy
-    end
+    it { expect(described_class.rates(target: target)).to be_truthy }
   end
 end
