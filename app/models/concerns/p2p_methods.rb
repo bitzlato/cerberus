2b924763e1f2a18540e6ea4cc494a949aa555d71
elnikov <elnikov.s@gmail.com>
2022-02-10 11:10:39 +0300

    [#181171559] outcome mvp

--- app/models/concerns/p2p_methods.rb
+++ app/models/concerns/p2p_methods.rb
@@ -6,5 +6,23 @@ module P2PMethods
       @p2p_user ||= BitzlatoUser.find(user_uid)
     end
 
+
+    def p2p_income
+
+    end
+
+
+    def p2p_outcome
+      outcome = BitzlatoWithdrawal.processed
+                                  .last_1_month
+                                  .where(user: p2p_user)
+                                  .select('sum(amount) as sum_amount, cc_code as currency_id')
+                                  .group('currency_id')
+                                  .map { |t| { "#{t.currency_id.upcase}" => t.sum_amount } }
+                                  .reduce({}, :merge)
+
+      {origin: outcome, converted: CurrencyConvert.convert(outcome)}
+    end
+
   end
 end
