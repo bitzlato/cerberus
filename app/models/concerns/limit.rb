ec18c9d523ce512d687d7586e016fb2d3c580e29
elnikov <elnikov.s@gmail.com>
2022-02-11 08:42:14 +0300

    [#181171559] refactor limits

--- app/models/concerns/limit.rb
+++ app/models/concerns/limit.rb
@@ -1,35 +1,30 @@
 module Limit
   extend ActiveSupport::Concern
   included do
-
     def income
-      result = {origin: {}, converted: {}}
-      incomes = []
+      incomes = [p2p_income, month_income_exchange]
+      summarize = summarize_hash_limits(incomes)
 
-      incomes << p2p_income
-      incomes << p2p_voucher_withdraw
-      incomes << month_income_exchange
-      incomes.map do |h|
-        result[:origin] = result[:origin].merge(h[:origin]){ |k, a_value, b_value| a_value + b_value }
-        result[:converted] = result[:converted].merge(h[:converted]){ |k, a_value, b_value| a_value + b_value }
-      end
-
-      result.with_indifferent_access
+      {p2p: incomes[0], exchange: incomes[1], summarize: summarize}.with_indifferent_access
     end
 
     def outcome
-      result = {origin: {}, converted: {}}
-      outcomes = []
+      outcomes =[ p2p_outcome, p2p_voucher_withdraw, month_outcome_exchange]
+      summarize = summarize_hash_limits(outcomes)
+      p2p_summarize = summarize_hash_limits([outcomes[0], outcomes[1]])
 
-      outcomes << p2p_outcome
-      outcomes << month_outcome_exchange
-      outcomes.map do |h|
+      {p2p: p2p_summarize, exchange: outcomes[1], summarize: summarize}.with_indifferent_access
+    end
+
+    private
+
+    def summarize_hash_limits(array_of_hash)
+      result = {origin: {}, converted: {}}
+      array_of_hash.map do |h|
         result[:origin] = result[:origin].merge(h[:origin]){ |k, a_value, b_value| a_value + b_value }
         result[:converted] = result[:converted].merge(h[:converted]){ |k, a_value, b_value| a_value + b_value }
       end
-
-      result.with_indifferent_access
+      result
     end
-
   end
 end
