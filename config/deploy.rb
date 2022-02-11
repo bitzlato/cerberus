572dbe523c82bb2380a09f635fe99b86b131a53d
elnikov <elnikov.s@gmail.com>
2022-02-11 14:38:29 +0300

    [#181171559] cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,6 +87,14 @@ if defined? Slackistrano
   set :slackistrano_footer_icon, 'https://github.githubassets.com/images/modules/logos_page/Octocat.png'
 end
 
+namespace :rake do
+  desc "Run a task on a remote server."
+  # run like: cap staging rake:invoke task=a_certain_task
+  task :invoke do
+    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")
+  end
+end
+
 # Removed rake, bundle, gem
 # Added rails.
 # rake has its own dotenv requirement in Rakefile
