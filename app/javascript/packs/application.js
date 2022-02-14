fdc74dd73d9b1026f8ab50b42f1cc7e3223009d3
elnikov <elnikov.s@gmail.com>
2022-02-14 11:15:05 +0300

    [#181171559] fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,35 +10,3 @@ import * as ActiveStorage from "@rails/activestorage"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-class RowElement extends HTMLTableRowElement {
-    connectedCallback() {
-        this.addEventListener('click', this.onClick)
-        this.style='cursor: pointer'
-    }
-
-    onClick(e) {
-        const $target = $(e.target)
-        if ( $target.closest('a').length > 0 ||
-            $target.is('.btn') ||
-            $target.is('input') ||
-            $target.is('select') ||
-            $target.is('.best_in_place') ||
-            $target.closest('.project-actions').length > 0 )
-        {
-            return
-        }
-
-        e.preventDefault()
-
-        if (this.dataset.hrefTarget) {
-            window.open(this.dataset.href, this.dataset.hrefTarget)
-        } else {
-            window.location = this.dataset.href
-        }
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-    customElements.define('dapi-tr', RowElement, {extends: 'tr'});
-});
