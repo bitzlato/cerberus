384d5201f2893a4b5c7ff97a50d6463ecc8ee406
elnikov <elnikov.s@gmail.com>
2022-02-16 16:16:23 +0300

    Feature/voucher limits (#18)
    * rename methods
    
    * add voucher income and outcome
    
    * add voucher income/outcome to applicant show
    
    * add not_deleted and period to voucher

--- app/models/concerns/p2p_methods.rb
+++ app/models/concerns/p2p_methods.rb
@@ -6,30 +6,40 @@ module P2PMethods
       @p2p_user ||= Bitzlato::User.find_by_id(user_uid)
     end
 
-    def p2p_voucher_withdraw(period: :monthly, start_date: nil, end_date: nil)
-      #TODO: непонятно это вводы или вывод
-      amount = Bitzlato::WithdrawVoucher.joins(:voucher)
-                                        .last_1_month
-                                        .where(user: p2p_user)
-                                        .select('sum(voucher.amount) as sum_amount, voucher.cc_code as currency_id')
-                                        .group(:currency_id)
-                                        .map { |t| { "#{t.currency_id.upcase}" => t.sum_amount } }
-                                        .reduce({}, :merge)
+    def p2p_voucher_outcome(period: :monthly)
+      amount = Bitzlato::Voucher.where('voucher.cashed_by_user_id is NULL and user_id = ?', p2p_user&.id)
+                                .period(period)
+                                .not_deleted
+                                .select('sum(amount) as sum_amount, cc_code as currency_id')
+                                .group(:currency_id)
+                                .map { |t| { "#{t.currency_id.upcase}" => t.sum_amount } }
+                                .reduce({}, :merge)
+
 
       {origin: amount, converted: CurrencyConvert.convert(amount)}
-    rescue StandardError => e
-      p e
-      {origin: {}, converted: {}}
     end
 
+    def p2p_voucher_income(period: :monthly)
+      amount = Bitzlato::Voucher.where('voucher.cashed_by_user_id = ?', p2p_user&.id)
+                                .period(period)
+                                .not_deleted
+                                .select('sum(amount) as sum_amount, cc_code as currency_id')
+                                .group(:currency_id)
+                                .map { |t| { "#{t.currency_id.upcase}" => t.sum_amount } }
+                                .reduce({}, :merge)
+
+      {origin: amount, converted: CurrencyConvert.convert(amount)}
+    end
+
+
     def p2p_income(period: :monthly)
-      income = Bitzlato::BitzlatoDeposit.success
-                                        .period(period)
-                                        .where(user: p2p_user)
-                                        .select('sum(fee) + sum(amount) as sum_amount , cc_code as currency_id')
-                                        .group('currency_id')
-                                        .map { |t| { "#{t.currency_id.upcase}" => t.sum_amount } }
-                                        .reduce({}, :merge)
+      income = Bitzlato::Deposit.success
+                                .period(period)
+                                .where(user: p2p_user)
+                                .select('sum(fee) + sum(amount) as sum_amount , cc_code as currency_id')
+                                .group('currency_id')
+                                .map { |t| { "#{t.currency_id.upcase}" => t.sum_amount } }
+                                .reduce({}, :merge)
 
       {origin: income, converted: CurrencyConvert.convert(income)}
     rescue StandardError => e
