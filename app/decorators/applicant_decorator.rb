51f3df7ef78b6ac4871ea68b086824761d0e917c
elnikov <elnikov.s@gmail.com>
2022-02-18 18:34:49 +0300

    codegen for applicant_decorator.rb

--- app/decorators/applicant_decorator.rb
+++ app/decorators/applicant_decorator.rb
@@ -8,139 +8,47 @@ class ApplicantDecorator < Draper::Decorator
                          'banned' => 'badge badge-danger',
                          'init' => 'badge badge-info', }
 
+  CURRENCY_SUMBOLS = { 'usdc': '$',
+                       'btc': '₿' }.with_indifferent_access
+
 
   def status_colored
     h.content_tag(:span, object.status, class: CSS_STATUS_CLASSES[object.status])
   end
 
-  ######
 
-  def monthly_usd_income
-    "#{income_monthly_lazy.dig(:summarize, :converted, 'USDC').round(2) rescue 0} $"
+  ['monthly', 'daily'].each do |period|
+    ['usdc', 'btc'].each do |currency|
+      ['income','outcome'].each do |direction|
+        ['p2p', 'exchange', 'summarize', 'voucher'].each do |from|
+
+          define_method "#{period}_#{currency}_#{from}_#{direction}" do
+            lazy = self.send("#{direction}_#{period}_lazy")
+            "#{lazy.dig(from.to_sym, :converted, currency.upcase).round(2)} #{CURRENCY_SUMBOLS[currency]}"
+          rescue
+            '-'
+          end
+        end
+      end
+    end
   end
-
-  def monthly_usd_exchange_income
-    "#{income_monthly_lazy.dig(:exchange, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def monthly_usd_p2p_income
-    "#{income_monthly_lazy.dig(:p2p, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def daily_usd_income
-    "#{income_daily_lazy.dig(:summarize, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def daily_usd_exchange_income
-    "#{income_daily_lazy.dig(:exchange, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def daily_usd_p2p_income
-    "#{income_daily_lazy.dig(:p2p, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-
-  #####################################
-
-  def monthly_usd_outcome
-    "#{outcome_monthly_lazy.dig(:summarize, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def monthly_usd_exchange_outcome
-    "#{outcome_monthly_lazy.dig(:exchange, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def monthly_usd_p2p_outcome
-    "#{outcome_monthly_lazy.dig(:p2p, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def daily_usd_outcome
-    "#{outcome_daily_lazy.dig(:summarize, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def daily_usd_exchange_outcome
-    "#{outcome_daily_lazy.dig(:exchange, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  def daily_usd_p2p_outcome
-    "#{outcome_daily_lazy.dig(:p2p, :converted, 'USDC').round(2) rescue 0} $"
-  end
-
-  #####################################
-  #####################################
-
-  def monthly_btc_outcome
-    "#{outcome_monthly_lazy.dig(:summarize, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def monthly_btc_exchange_outcome
-    "#{outcome_monthly_lazy.dig(:exchange, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def monthly_btc_p2p_outcome
-    "#{outcome_monthly_lazy.dig(:p2p, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def daily_btc_outcome
-    "#{outcome_daily_lazy.dig(:summarize, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def daily_btc_exchange_outcome
-    "#{outcome_daily_lazy.dig(:exchange, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def daily_btc_p2p_outcome
-    "#{outcome_daily_lazy.dig(:p2p, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-
-  #####################################
-
-  def monthly_btc_income
-    "#{income_monthly_lazy.dig(:summarize, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def monthly_btc_exchange_income
-    "#{income_monthly_lazy.dig(:exchange, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def monthly_btc_p2p_income
-    "#{income_monthly_lazy.dig(:p2p, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-
-  def daily_btc_income
-    "#{income_daily_lazy.dig(:summarize, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def daily_btc_exchange_income
-    "#{income_daily_lazy.dig(:exchange, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  def daily_btc_p2p_income
-    "#{income_daily_lazy.dig(:p2p, :converted, 'BTC').round(4) rescue 0} ₿"
-  end
-
-  #####################################
-  #####################################
-
 
   private
 
   def income_monthly_lazy
-    @income_lazy ||= object.income(period: :monthly)
+    @income_monthly_lazy ||= object.income(period: :monthly)
   end
 
   def outcome_monthly_lazy
-    @outcome_lazy ||= object.outcome(period: :monthly)
+    @outcome_monthly_lazy ||= object.outcome(period: :monthly)
   end
 
-  def income_monthly_daily
-    @income_lazy ||= object.income(period: :daily)
+  def income_daily_lazy
+    @income_daily_lazy ||= object.income(period: :daily)
   end
 
-  def outcome_monthly_daily
-    @outcome_lazy ||= object.outcome(period: :daily)
+  def outcome_daily_lazy
+    @outcome_daily_lazy ||= object.outcome(period: :daily)
   end
 
 end
