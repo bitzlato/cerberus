6c8f61f64b27e649558463f08ffde7f71a6d3ca4
elnikov <elnikov.s@gmail.com>
2022-02-21 17:30:17 +0300

    fix specs

--- spec/services/sumsub/generate_url_spec.rb
+++ spec/services/sumsub/generate_url_spec.rb
@@ -6,43 +6,13 @@ describe SumSub::GenerateUrl do
     before do
       described_class.clear_cache
       @public_id = rand(99999)
-      stub_websdk_link(sumsub_applicant_id: @public_id)
+      stub_websdk_link
     end
 
     it 'ok' do
-      klass = described_class.new(sumsub_applicant_id: @public_id)
+      klass = described_class.new(public_id: @public_id)
       url = klass.call
       expect(url.include? 'https://api.sumsub.com/idensic/').to be true
     end
-  end
-
-  describe 'cache url' do
-    before do
-      @public_id = rand(99999)
-      stub_websdk_link(sumsub_applicant_id: @public_id)
-    end
-    it 'ok' do
-      described_class.clear_cache
-      generate_url =  described_class.new(sumsub_applicant_id: @public_id)
-      url1 = generate_url.call
-
-      stub_websdk_link(sumsub_applicant_id: @public_id)
-      generate_url =  described_class.new(sumsub_applicant_id: @public_id)
-      url2 = generate_url.call
-      expect(url1).to eq(url2)
-
-      described_class.clear_cache
-      stub_websdk_link(sumsub_applicant_id: @public_id)
-      generate_url =  described_class.new(sumsub_applicant_id: @public_id)
-      url3 = generate_url.call
-      expect(url2).not_to eq(url3)
-
-      Timecop.travel(Time.now+described_class::CACHE_TIME)
-      stub_websdk_link(sumsub_applicant_id: @public_id)
-      generate_url =  described_class.new(sumsub_applicant_id: @public_id)
-      url4 = generate_url.call
-      expect(url3).not_to eq(url4)
-    end
-
   end
 end
