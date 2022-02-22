5b84bc7ad8722a910d79ce80028ab86df2e355b0
elnikov <elnikov.s@gmail.com>
2022-02-22 18:07:24 +0300

    [#181342829] add paginate

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -1,9 +1,19 @@
 class Admin::ApplicantsController < Admin::AdminController
   def index
-    @applicants = Applicant.all.decorate
+    @applicants = Applicant.all.page(page).per(per_page || 15).decorate
   end
 
   def show
     @applicant = Applicant.find(params[:id]).decorate
+  end
+
+  private
+
+  def per_page
+    params[:per]
+  end
+
+  def page
+    params[:page]
   end
 end
