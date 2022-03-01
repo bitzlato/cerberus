fafb567cbcbf0dec7ab07ddfb599bf5db65ad233
elnikov <elnikov.s@gmail.com>
2022-03-01 11:53:50 +0300

    upload

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -3,12 +3,27 @@ class VerificationsController < ApplicationController
     @verifications = Verification.all.page(page).per(per_page).decorate
   end
 
+  def show
+    @verification = Verification.find(params[:id])
+  end
+
   def new
+    @verification = Verification.new
   end
 
   def create
+    @verification = Verification.new(verifcation_params)
+    if @verification.save
+      redirect_to @verification
+    else
+      render 'new'
+    end
   end
 
   def update
+  end
+
+  def verifcation_params
+    params.require(:form).permit(:country, {documents: []})
   end
 end
