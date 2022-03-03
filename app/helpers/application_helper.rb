2c5443dcd69bd0caf5385e5741847c680c387e4f
elnikov <elnikov.s@gmail.com>
2022-03-03 16:20:54 +0300

    image or video tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,18 @@
 module ApplicationHelper
 
+  VIDEO_EXTS = %w[.mp4 .mov]
+
   def back_link(url = nil)
     link_to ('&larr; ' + t('.back')).html_safe, url || root_path
+  end
+
+  def image_or_video(url)
+    ext = File.extname(url).downcase
+    if VIDEO_EXTS.include? ext
+      tag.video(tag.source(src: url), controls: 'controls')
+    else
+      image_tag url
+    end
   end
 
 end
